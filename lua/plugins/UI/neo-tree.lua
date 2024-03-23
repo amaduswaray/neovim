8a2363676da6ae74965fe30cbc8f73f568aa55bb
Amadu Swaray <amaduswaray06@gmail.com>
2024-03-23 01:13:23 +0100

    First draft done

--- lua/plugins/UI/neo-tree.lua
+++ lua/plugins/UI/neo-tree.lua
@@ -1,13 +1,53 @@
 return {
-   "nvim-neo-tree/neo-tree.nvim",
-   branch = "v3.x",
-   dependencies = {
-     "nvim-lua/plenary.nvim",
-     "nvim-tree/nvim-web-devicons",
-     "MunifTanjim/nui.nvim",
-   },
-   config = function()
-     vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left toggle<CR>')
-   end,
-   }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      default_component_configs = {
+        container = {
+          enable_character_fade = true
+        },
+        indent = {
+          indent_size = 2,
+          padding = 1, -- extra padding on left hand side
+          -- indent guides
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+          -- expander config, needed for nesting files
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "￫",
+          expander_expanded = "ꜜ",
+          expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "󰜌",
+          -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+          -- then these will never be used.
+          default = "*",
+          highlight = "NeoTreeFileIcon"
+        },
+        modified = {
+          symbol = " ",
+          highlight = "NeoTreeModified",
+        },
+        name = {
+          trailing_slash = false,
+          use_git_status_colors = true,
+          highlight = "NeoTreeFileName",
+        },
+      }
 
+    })
+    -- Keymaps
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left toggle<CR>')
+    end,
+    }
