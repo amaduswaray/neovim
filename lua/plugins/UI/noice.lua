53528a5f9249987067ee064878fbcc1a3c79a98e
Amadu Swaray <amaduswaray06@gmail.com>
2024-03-23 19:45:37 +0100

    Removed noice command when opening file tree

--- lua/plugins/UI/noice.lua
+++ lua/plugins/UI/noice.lua
@@ -1,61 +1,61 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
---    "rcarriga/nvim-notify",
-    },
-  config = function ()
-    local noice = require("noice")
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		--    "rcarriga/nvim-notify",
+	},
+	config = function()
+		local noice = require("noice")
 
-    noice.setup({
-      lsp = {
-        -- Ovveride markdown rendering
-        ovveride = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
+		noice.setup({
+			lsp = {
+				-- Ovveride markdown rendering
+				ovveride = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
 
-      -- UI for routes an changes
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
+			-- UI for routes an changes
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "mini",
+				},
+			},
 
-      -- Noice presets
-      presets = {
-        bottom_search = true, -- use classic cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an unout dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signture help
-      },
+			-- Noice presets
+			presets = {
+				bottom_search = true, -- use classic cmdline for search
+				command_palette = false, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an unout dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signture help
+			},
 
-      -- How to display messages
-      messages = {
-        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-        -- This is a current Neovim limitation.
-        enabled = true, -- enables the Noice messages UI
-        view = "mini", -- default view for messages
-        view_error = "mini", -- view for errors
-        view_warn = "mini", -- view for warnings
-        view_history = "messages", -- view for :messages
-        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-      },
-    })
-  end
+			-- How to display messages
+			messages = {
+				-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+				-- This is a current Neovim limitation.
+				enabled = true, -- enables the Noice messages UI
+				view = "mini", -- default view for messages
+				view_error = "mini", -- view for errors
+				view_warn = "mini", -- view for warnings
+				view_history = "messages", -- view for :messages
+				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+			},
+		})
+	end,
 }
