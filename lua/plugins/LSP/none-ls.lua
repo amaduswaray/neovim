181f0ddb3100515f2a744698d80b130a049b6026
Amadu Swaray <amaduswaray06@gmail.com>
2024-03-23 19:37:52 +0100

    Added formatting on save(complete)

--- lua/plugins/LSP/none-ls.lua
+++ lua/plugins/LSP/none-ls.lua
@@ -1,66 +1,41 @@
 return {
-  "nvimtools/none-ls.nvim",
-  lazy = true,
-  -- event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
-  dependencies = {
-    "jay-babu/mason-null-ls.nvim",
-  },
-  config = function()
-    local mason_null_ls = require("mason-null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    local null_ls = require("null-ls")
+		-- Format on save
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    local null_ls_utils = require("null-ls.utils")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
+			},
 
-    mason_null_ls.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "eslint_d", -- js linter
-      },
-    })
+			-- configure format on save
+			on_attach = function(current_client, bufnr)
+				if current_client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								filter = function(client)
+									--  only use null-ls for formatting instead of lsp server
+									return client.name == "null-ls"
+								end,
+								bufnr = bufnr,
+							})
+						end,
+					})
+				end
+			end,
+		})
 
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
-
-    -- Format on save
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    -- configure null_ls
-    null_ls.setup({
-      -- add package.json as identifier for root (for typescript monorepos)
-      root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-      -- setup formatters & linters
-      sources = {
-        --  to disable file types use
-        --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-        formatting.prettier,
-        formatting.stylua,
-        diagnostics.eslint_d.with({
-          condition = function(utils)
-            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" })
-          end,
-        }),
-      },
-      -- configure format on save
-      on_attach = function(current_client, bufnr)
-        if current_client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                filter = function(client)
-                  --  only use null-ls for formatting instead of lsp server
-                  return client.name == "null-ls"
-                end,
-                bufnr = bufnr,
-              })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
