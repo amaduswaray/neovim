6b168ba768bd3eb1b0731fe65fd9ce1bab128ea6
Amadu Swaray <amaduswaray06@gmail.com>
2024-03-25 15:00:29 +0100

    Clean bufferline(AMAVIM v1.1)

--- lua/plugins/LSP/lspconfig.lua
+++ lua/plugins/LSP/lspconfig.lua
@@ -72,7 +72,7 @@ return {
 		-- LSP autocompletion
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- lua/plugins/UI/bufferline.lua
+++ lua/plugins/UI/bufferline.lua
@@ -1,47 +1,51 @@
-
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  keys = {
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-  },
-  opts = {
-    options = {
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	keys = {
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+	},
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
       -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
       -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-      diagnostics = "nvim_lsp",
-      always_show_bufferline = false,
-      diagnostics_indicator = function(_, _, diag)
-      local icons = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warn .. diag.warning or "")
-        return vim.trim(ret)
-      end,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "Neo-tree",
-          highlight = "Directory",
-          text_align = "left",
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    require("bufferline").setup(opts)
-    -- Fix bufferline when restoring a session
-    vim.api.nvim_create_autocmd("BufAdd", {
-      callback = function()
-        vim.schedule(function()
-          pcall(nvim_bufferline)
-        end)
-      end,
-    })
-  end,
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(_, _, diag)
+					local icons = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+				style_preset = {
+					bufferline.style_preset.no_italic,
+					bufferline.style_preset.no_bold,
+					bufferline.style_preset.minimal,
+				},
+			},
+		})
+		-- Fix bufferline when restoring a session
+		vim.api.nvim_create_autocmd("BufAdd", {
+			callback = function()
+				vim.schedule(function()
+					pcall(nvim_bufferline)
+				end)
+			end,
+		})
+	end,
 }
