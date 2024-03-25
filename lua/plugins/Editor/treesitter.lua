3aa57b0c137031b53d5aa23be54b0b523306169e
Amadu Swaray <amaduswaray06@gmail.com>
2024-03-25 17:43:19 +0100

    Added markdown inline
    Smoother markdown note taking experience

--- lua/plugins/Editor/treesitter.lua
+++ lua/plugins/Editor/treesitter.lua
@@ -1,15 +1,15 @@
-
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "markdown_inline" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
