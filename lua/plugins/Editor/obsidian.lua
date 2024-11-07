08e26bc4fbbff0c648d7b1393476637fa00fe708
Amadu Swaray <amaduswaray06@gmail.com>
2024-11-07 18:53:55 +0100

    Predefined metadata stays on the note

--- lua/plugins/Editor/obsidian.lua
+++ lua/plugins/Editor/obsidian.lua
@@ -2,6 +2,15 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
+	keys = {
+		{
+			"<leader>cd",
+			"<cmd>ObsidianToday<cr>",
+			desc = "Obsidian Daily Note Today",
+			-- icon = { icon = "󰍲", color = "oeange" },
+		},
+		{ "<leader>ct", "<cmd>ObsidianTemplate<cr>", desc = "Obsidian Insert Template" },
+	},
 	-- ft = "markdown", -- Activate this and remove event if you want the plugin to apply to all md files
 	event = {
 		"BufReadPre /Users/amaduswaray/Library/Mobile Documents/iCloud~md~obsidian/Documents/UiO/*.md",
@@ -19,12 +28,12 @@ return {
 			},
 		},
 		daily_notes = {
-			folder = "/Users/amaduswaray/Library/Mobile Documents/iCloud~md~obsidian/Documents/UiO/Z - Daily Notes",
+			folder = "Z - Daily Notes",
 			date_format = "%Y-%m-%d",
 			template = "Templates/Daily.md",
 		},
 		templates = {
-			foler = "/Users/amaduswaray/Library/Mobile Documents/iCloud~md~obsidian/Documents/UiO/Templates/",
+			foler = "Templates",
 			date_format = "%Y-%m-%d",
 			time_format = "%H:%M",
 		},
@@ -36,24 +45,16 @@ return {
 				end,
 				opts = { buffer = true, desc = "Obsidian Toggle Checkbox" },
 			},
-			-- Create new daily Notes
-			["<leader>cd"] = {
-				action = function()
-					return require("obsidian").util.today()
-				end,
-				opts = { buffer = false, desc = "Obsidian Daily Note Today" },
-			},
-			["<leader>ct"] = {
-				action = function()
-					return require("obsidian").util.template()
-				end,
-				opts = { buffer = true, desc = "Obsidian Insert Template" },
-			},
 		},
 		-- Remove the metadata formatting from the plugin
 		---@return table
 		note_frontmatter_func = function(note)
 			local out = {}
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
 
 			return out
 		end,
