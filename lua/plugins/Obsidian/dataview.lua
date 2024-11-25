0e5f10b5cda09dcbb50a76af8f9727698ef98bf4
Amadu Swaray <amaduswaray06@gmail.com>
2024-11-25 14:40:15 +0100

    Attempted dataview fix

--- lua/plugins/Obsidian/dataview.lua
+++ lua/plugins/Obsidian/dataview.lua
@@ -9,9 +9,9 @@ return {
 	-- configuration here, see below for full configuration options
 	opts = {
 		vault_dir = "/Users/amaduswaray/Library/Mobile Documents/iCloud~md~obsidian/Documents/UiO",
-		buffer_type = "tab", -- float | split | vsplit | tab
+		buffer_type = "float", -- float | split | vsplit | tab
 	},
-	-- config = function()
-	-- 	require("dataview").setup(opts)
-	-- end,
+	config = function()
+		require("dataview").setup(opts)
+	end,
 }
