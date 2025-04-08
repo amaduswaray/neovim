57f476b0875e442f9a4ff5cb21fa87c78596ffe4
Amadu Swaray <amaduswaray06@gmail.com>
2025-04-08 14:01:20 +0200

    Lazy vims neotree
    Still need option for opening from current file

--- lua/plugins/UI/neo-tree.lua
+++ lua/plugins/UI/neo-tree.lua
@@ -6,11 +6,44 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	keys = { {
+	cmd = "Neotree",
+	keys = {
+		{
+			"<leader>fe",
+			function()
+				require("neo-tree.command").execute({ toggle = true })
+			end,
+			desc = "Explorer NeoTree (Root Dir)",
+		},
+		{
+			"<leader>fE",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+			end,
+			desc = "Explorer NeoTree (cwd)",
+		},
+		{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+		{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+		{
+			"<leader>ge",
+			function()
+				require("neo-tree.command").execute({ source = "git_status", toggle = true })
+			end,
+			desc = "Git Explorer",
+		},
+		{
+			"<leader>be",
+			function()
+				require("neo-tree.command").execute({ source = "buffers", toggle = true })
+			end,
+			desc = "Buffer Explorer",
+		},
+	},
+	--[[ keys = { {
 		"<leader>e",
 		"<cmd>Neotree toggle<cr>",
 		desc = "File Explorer - Toggle",
-	} },
+	} }, ]]
 
 	config = function()
 		-- Local highlights for file
