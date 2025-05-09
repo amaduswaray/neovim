b615cfb10b3d2061c4821807c86c82bd941d34d4
Amadu Swaray <amaduswaray@Amadus-MacBook-Pro.local>
2025-05-09 19:19:26 +0200

    Added description to formatting

--- lua/plugins/LSP/none-ls.lua
+++ lua/plugins/LSP/none-ls.lua
@@ -42,6 +42,6 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
 	end,
 }
